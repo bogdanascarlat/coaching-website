535d494e4582891353ad5f8962d2cbd1900c73d7
Scarlat Bogdana <bogdana.scarlat@gmail.com>
2023-10-25 10:53:39 +0300

    changed Podcst style

--- src/pages/Events/Podcast.js
+++ src/pages/Events/Podcast.js
@@ -7,14 +7,28 @@ const Podcast = () => {
     <div>
       <Navbar />
       <div className="flex justify-center">
-        <div className="grid grid-cols-1 gap-20 md:grid-cols-2 mt-[6rem] mb-[6rem] items-center justify-center">
-          <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/7Nkhi3Y0mRg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-          <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/QbgTg4e-nzA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-          <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/yktl6S207io" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-          <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/QQDB5NEGA-E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-          <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/aaY7dbxVPLs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-          <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/E0GiLdYhDuI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-          <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/0UOc1uKFkyM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <div className="mx-4 grid grid-cols-1 gap-36 md:grid-cols-2 mt-[6rem] mb-[6rem] items-center justify-center">
+          <div className="mx-auto">
+            <iframe className="w-[56] md:w-[30rem] h-56 md:h-64 lg:h-72 xl:h-80" src="https://www.youtube-nocookie.com/embed/7Nkhi3Y0mRg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          </div>
+          <div className="mx-auto">
+            <iframe className="w-[56] md:w-[30rem] h-56 md:h-64 lg:h-72 xl:h-80" src="https://www.youtube-nocookie.com/embed/QbgTg4e-nzA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          </div>
+          <div className="mx-auto">
+            <iframe className="w-[56] md:w-[30rem] h-56 md:h-64 lg:h-72 xl:h-80" src="https://www.youtube-nocookie.com/embed/yktl6S207io" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          </div>
+          <div className="mx-auto">
+            <iframe className="w-[56] md:w-[30rem] h-56 md:h-64 lg:h-72 xl:h-80" src="https://www.youtube-nocookie.com/embed/QQDB5NEGA-E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          </div>
+          <div className="mx-auto">
+            <iframe className="w-[56] md:w-[30rem] h-56 md:h-64 lg:h-72 xl:h-80" src="https://www.youtube-nocookie.com/embed/aaY7dbxVPLs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          </div>
+          <div className="mx-auto">
+            <iframe className="w-[56] md:w-[30rem] h-56 md:h-64 lg:h-72 xl:h-80" src="https://www.youtube-nocookie.com/embed/E0GiLdYhDuI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          </div>
+          <div className="mx-auto">
+            <iframe className="w-[56] md:w-[30rem] h-56 md:h-64 lg:h-72 xl:h-80" src="https://www.youtube-nocookie.com/embed/0UOc1uKFkyM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          </div>
         </div>
       </div>  
       <Footer />
